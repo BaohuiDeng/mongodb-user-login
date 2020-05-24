a96c4ea8b21541cccc9903aea752a42b738b2a6e
BaohuiDeng <46564403+BaohuiDeng@users.noreply.github.com>
2020-05-24 15:46:50 +0200

    Update login_example.py

--- login_example.py
+++ login_example.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, url_for, request, session, redirect
-from flask.ext.pymongo import PyMongo
+from flask_pymongo import PyMongo 
 import bcrypt
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'mongologinexample'
-app.config['MONGO_URI'] = 'mongodb://pretty:printed@ds021731.mlab.com:21731/mongologinexample'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mongologinexample'
 
 mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    if 'username' in session:
-        return 'You are logged in as ' + session['username']
+    #if 'username' in session:
+     #   return 'You are logged in as ' + session['username']
 
     return render_template('index.html')
 
@@ -22,9 +22,9 @@ def login():
     login_user = users.find_one({'name' : request.form['username']})
 
     if login_user:
-        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
             session['username'] = request.form['username']
-            return redirect(url_for('index'))
+            return 'You are logged in as ' + session['username']
 
     return 'Invalid username/password combination'
 
@@ -38,7 +38,7 @@ def register():
             hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
             users.insert({'name' : request.form['username'], 'password' : hashpass})
             session['username'] = request.form['username']
-            return redirect(url_for('index'))
+            return 'you are successfully registered'
         
         return 'That username already exists!'
 
